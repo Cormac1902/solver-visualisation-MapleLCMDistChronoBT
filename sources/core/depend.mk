2b104877643bbb6f232ad740d43f4acf6af29e6b
Cormac Geraghty <CormacGeraghty@outlook.ie>
2020-09-14 12:38:07 +0100

    First commit

---
+++ sources/core/depend.mk
@@ -0,0 +1,12 @@
+/home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.o /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.or /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.od /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.op: \
+ /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.cc \
+ ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+ ../core/Solver.h ../mtl/Heap.h ../mtl/Alg.h ../utils/Options.h \
+ ../utils/ParseUtils.h ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h # \
+ # /usr/include/s_vis_zmq.hpp
+/home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.o /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.or /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.od /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.op: \
+ /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.cc \
+ ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+ ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+ ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+ ../core/Solver.h ../mtl/Heap.h
---
+++ sources/simp/depend.mk
@@ -0,0 +1,24 @@
+/home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/SimpSolver.o /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/SimpSolver.or /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/SimpSolver.od /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/SimpSolver.op: \
+ /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/SimpSolver.cc \
+ ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+ ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
+ ../mtl/Alg.h ../utils/Options.h ../utils/ParseUtils.h \
+ ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h ../utils/System.h
+/home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/Main.o /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/Main.or /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/Main.od /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/Main.op: \
+ /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/simp/Main.cc \
+ ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+ ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+ ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+ ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h
+/home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.o /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.or /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.od /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.op: \
+ /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Solver.cc \
+ ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+ ../core/Solver.h ../mtl/Heap.h ../mtl/Alg.h ../utils/Options.h \
+ ../utils/ParseUtils.h ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h # \
+ # /usr/include/s_vis_zmq.hpp
+/home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.o /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.or /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.od /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.op: \
+ /home/cormac/OneDrive/Family/Cormac/College/NUIG/Masters/Solvers/MapleLCMDistChronoBT/sources/core/Main.cc \
+ ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+ ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+ ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+ ../core/Solver.h ../mtl/Heap.h
